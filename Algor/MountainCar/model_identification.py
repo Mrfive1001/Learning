e0ceac674475bd0809fe6e44dcea2ab7a944e927
MrFive <13070156190@163.com>
2018-09-11 17:06:07 +0800

    修改逻辑

--- Algor/MountainCar/model_identification.py
+++ Algor/MountainCar/model_identification.py
@@ -5,17 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-def get_my_dot(X, u, net):
-    '''
-    :param X: 状态变量
-    :param net:  神经网络，可以进行预测
-    :return: 返回与X维度相同的导数,和神经网络预测结果
-    '''
-    B = np.array([0.0015])
-    d1 = net.predict(X)[0]
-    d2 = B * u
-    return d1 + d2, d1
-
 
 def step_my(X, u, net):
     '''
@@ -23,16 +12,22 @@ def step_my(X, u, net):
     :param net:  神经网络，可以进行预测
     :return: 返回下个状态，和神经网络预测结果
     '''
-    simulation_step = 1  # 仿真步长
+    simulation_step = 0.1  # 仿真步长
     u = min(max(u, -1.0), 1.0)
     x, v = X
-    dot, d = get_my_dot(X, u, net)
-    v = v + simulation_step * dot[0]
+    dot = net.predict(X)[0]
+
+    # 预测导数
+    v_dot = 0.0015 * u + dot[0]
+    v = v + v_dot * simulation_step
     v = min(max(v, -0.07), 0.07)
+
     x = x + simulation_step * v
     x = min(max(x, -1.2), 0.6)
-    X = np.array([x, v])
-    return X, d
+    X = np.array([x,v])
+    if X.ndim == 2:
+        X = X.reshape((2,))
+    return X,dot
 
 
 def get_error(error, X1, X2, k=0.1):
@@ -40,9 +35,9 @@ def get_error(error, X1, X2, k=0.1):
     通过两个状态得到导数之间的误差
     '''
     dot = k * (X1 - X2)
-    simulation_step = 1  # 仿真步长
+    simulation_step = 0.1  # 仿真步长
     error = error + simulation_step * dot
-    return error[0]
+    return error[1]
 
 
 env = gym.make("MountainCarContinuous-v0")
@@ -51,34 +46,40 @@ net = DNN(2, 1, 20, name='MyModel', train=train, memory_size=200)
 true_x = []
 pre_x = []
 if train:
-    for w in range(200):
+    record_error = []
+    for w in range(2):
         error = 0
         observation = env.reset()
-        for epi in range(500):
+        for epi in range(5000):
             u = env.action_space.sample()
             my_observation, d = step_my(observation, u, net)
             observation, reward, done, info = env.step(u)
             error = get_error(error, observation, my_observation)
             y = d + error
+            record_error.append(error)
             net.store_sample(observation, y)
             result = net.learn()
-            if epi % 50 == 0:
-                error = 0
             if result:
                 print(w, result)
     net.store_net()
 true_x = []
 pre_x = []
+
 observation = env.reset()
-for epi in range(500):
+for epi in range(5000):
     u = env.action_space.sample()
     my_observation, d = step_my(observation, u, net)
     observation, reward, done, info = env.step(u)
-    true_x.append(observation[0])
-    pre_x.append(my_observation[0])
+    true_x.append(observation[1])
+    pre_x.append(my_observation[1])
     result = net.learn()
     if result:
         print(w, result)
 plt.plot(true_x,'r')
 plt.plot(pre_x,'g')
+x_ = np.arange(len(record_error))
+fig = plt.figure()
+
+plt.plot(x_, record_error, label='v')
+plt.legend()
 plt.show()
---
+++ Algor/MountainCar/model_vertify.py
@@ -0,0 +1,81 @@
+import numpy as np
+import gym
+from dnn import DNN
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+
+sns.set()
+
+
+def step_my(X, u, mu):
+    '''
+    输入状态量和控制量和误差
+    返回下一个状态
+    '''
+    simulation_step = 0.1  # 仿真步长
+    u = min(max(u, -1.0), 1.0)
+    x, v = X
+
+    # 预测导数
+    v_dot = 0.0015 * u + mu
+    v = v + v_dot * simulation_step
+    v = min(max(v, -0.07), 0.07)
+
+    x = x + simulation_step * v
+    x = min(max(x, -1.2), 0.6)
+    X = np.array([x, v])
+    if X.ndim == 2:
+        X = X.reshape((2,))
+    return X
+
+
+def get_mu(mu, X1, X2, k=1):
+    '''
+    通过两个状态得到导数之间的误差
+    '''
+    dot = k * (X1 - X2)
+    simulation_step = 0.1  # 仿真步长
+    mu = mu + simulation_step * dot
+    return mu
+
+
+env = gym.make("MountainCarContinuous-v0")
+record_tru = []
+record_pre = []
+record_mu = []
+record_mu_tru = []
+observation = env.reset()
+mu = [0,0]
+for epi in range(50000):
+    u = env.action_space.sample()
+    my_observation = step_my(observation, u, mu[1])
+    observation, reward, done, info = env.step(u)
+    mu = get_mu(mu, observation, my_observation)
+    record_mu_tru.append(-math.cos(observation[0] * 3) * 0.0025)
+    record_tru.append(observation)
+    record_pre.append(my_observation)
+    record_mu.append(list(mu))
+    print(epi, observation, my_observation)
+    if done:
+        break
+record_tru = np.array(record_tru)
+record_pre = np.array(record_pre)
+record_mu = np.array(record_mu)
+record_mu_tru = np.array(record_mu_tru)
+x_ = np.arange(len(record_pre))
+fig = plt.figure()
+axis = 0
+plt.plot(x_, record_tru[:, axis], label='x_tru')
+plt.plot(x_, record_pre[:, axis], label='x_pre')
+plt.legend()
+fig = plt.figure()
+axis = 1
+plt.plot(x_, record_tru[:, axis], label='v_tru')
+plt.plot(x_, record_pre[:, axis], label='v_pre')
+plt.legend()
+fig = plt.figure()
+plt.plot(x_, record_mu[:, 1], label='mu_pre')
+plt.plot(x_, record_mu_tru, label='mu_tru')
+plt.legend()
+plt.show()
